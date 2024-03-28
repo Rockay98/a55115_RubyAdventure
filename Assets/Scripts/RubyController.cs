8212711de423fde78973421ffb3f4406ab85ad64
Rockay98 <filipepereira9855@gmail.com>
2024-03-28 17:24:52 +0000

    Add a Health Stat to Ruby

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
     Rigidbody2D rigidbody2D;
     float horizontal;
     float vertical;
     // Start is called before the first frame update
     void Start()
     {
-       rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -30,4 +35,11 @@ public class RubyController : MonoBehaviour
         
         rigidbody2D.MovePosition(position);
     }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
